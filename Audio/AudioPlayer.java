ca633a8ec7ea7b34972c8898260e4ed11228880e
shilpamurthy <shilpamurthy93@gmail.com>
2015-03-19 17:39:11 -0400

    Audio files

---
+++ Audio/AudioPlayer.java
@@ -0,0 +1,105 @@
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+
+public class AudioPlayer {
+	
+	public static void playAudio(boolean cup, boolean cup1, boolean cup2, boolean cup3,
+						float volume, float volume1, float volume2, float volume3){
+		
+		try{
+		
+			AudioInputStream audioInputStream =
+		        AudioSystem.getAudioInputStream( new File("test.wav"));
+		    Clip clip = AudioSystem.getClip();
+		    clip.open(audioInputStream);
+		    long sec = clip.getMicrosecondLength();
+		    long millisec = sec/1000;
+		    
+		    AudioInputStream audioInputStream1 =
+			        AudioSystem.getAudioInputStream( new File("test1.wav"));
+			Clip clip1 = AudioSystem.getClip();
+			clip1.open(audioInputStream1);
+			long sec1 = clip1.getMicrosecondLength();
+			long millisec1 = sec1/1000;
+			
+			AudioInputStream audioInputStream2 =
+				    AudioSystem.getAudioInputStream( new File("test2.wav"));
+			Clip clip2 = AudioSystem.getClip();
+			clip2.open(audioInputStream2);
+			long sec2 = clip2.getMicrosecondLength();
+			long millisec2 = sec2/1000;
+				    
+			AudioInputStream audioInputStream3 =
+				AudioSystem.getAudioInputStream( new File("test3.wav"));
+			Clip clip3 = AudioSystem.getClip();
+			clip3.open(audioInputStream3);
+			long sec3 = clip3.getMicrosecondLength();
+			long millisec3 = sec3/1000;
+			
+			
+			while (true){
+				
+		    	if (cup){
+		    		FloatControl gainControl = 
+			    		    (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+			    		gainControl.setValue(volume);
+		    		clip.start();
+		    	}
+		    	if (cup1){
+		    		FloatControl gainControl1 = 
+			    		    (FloatControl) clip1.getControl(FloatControl.Type.MASTER_GAIN);
+			    		gainControl1.setValue(volume1);
+		    		clip1.start();
+		    	}
+		    	if (cup2){
+		    		FloatControl gainControl2 = 
+			    		    (FloatControl) clip2.getControl(FloatControl.Type.MASTER_GAIN);
+			    		gainControl2.setValue(volume2);
+		    		clip2.start();
+		    	}
+		    	if (cup3){
+		    		FloatControl gainControl3 = 
+			    		    (FloatControl) clip3.getControl(FloatControl.Type.MASTER_GAIN);
+			    		gainControl3.setValue(volume3);
+		    		clip3.start();
+		    	}
+		    	
+		    	Thread.sleep(Math.min(millisec, Math.min(millisec1, Math.min(millisec2, millisec3))));
+		    
+		    	if (cup){
+		    		clip.stop();
+		    		clip.setFramePosition(0);
+		    	}
+		    	if (cup1){
+		    		clip1.stop();
+		    		clip1.setFramePosition(0);
+		    	}
+		    	if (cup2){
+		    		clip2.stop();
+		    		clip2.setFramePosition(0);
+		    	}
+		    	if (cup3){
+		    		clip3.stop();
+		    		clip3.setFramePosition(0);
+		    	}
+		    }
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	public static void main (String[] args){
+		
+		playAudio(true, false, true, true, -5.0f, 10.0f, 0.0f, 5.0f);
+		
+		
+	}
+
+}
