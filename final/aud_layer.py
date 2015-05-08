0a110988dbef39ec5cd85f69744d8d54ace48307
shilpamurthy <shilpamurthy93@gmail.com>
2015-05-08 00:19:38 -0400

    notes

--- final/aud_layer.py
+++ final/aud_layer.py
@@ -10,11 +10,11 @@ import time
 pfrequency1 = 130.82
 pfrequency2 = 329.63
 pfrequency3 = 659.26
-pfrequency4 = 783.99
+pfrequency4 = 1046.5
 frequency1 = 130.82
 frequency2 = 329.63
 frequency3 = 659.26
-frequency4 = 783.99
+frequency4 = 1046.5
 volume = 1
 cup1 = 0
 cup2 = 0
